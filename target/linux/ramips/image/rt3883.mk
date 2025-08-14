5e03b88183d1db7e1506b20dfa269e88dcce46a2
rikka0w0 <929513338@qq.com>
2025-08-15 05:17:29 +1000

    Add support for DLink DAP-1522-B1
    Based on 24.10.2

--- target/linux/ramips/image/rt3883.mk
+++ target/linux/ramips/image/rt3883.mk
@@ -30,6 +30,21 @@ define Device/belkin_f9k1109v1
 endef
 TARGET_DEVICES += belkin_f9k1109v1
 
+define Device/dlink_dap-1522-b1
+  $(Device/uimage-lzma-loader)
+  SOC := rt3662
+  IMAGE_SIZE := 7872k
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-kernel | append-rootfs
+  DEVICE_VENDOR := D-Link
+  DEVICE_MODEL := DAP-1522
+  DEVICE_VARIANT := B1
+  DEVICE_PACKAGES := uboot-envtools
+  FEATURES := $(filter-out usb,$(FEATURES))
+  SUPPORTED_DEVICES += dap-1522-b1
+endef
+TARGET_DEVICES += dlink_dap-1522-b1
+
 define Device/dlink_dir-645
   $(Device/seama-lzma-loader)
   SOC := rt3662
